ac1a5a95a35f27ebfae1e6ce597b9bb9306a451f
omerfdev <omerfdev@gmail.com>
2023-08-31 13:46:10 +0300

    Commit

--- VerifyEmailForgotPassword/Controllers/Models/UserRegisterRequest.cs
+++ VerifyEmailForgotPassword/Controllers/Models/UserRegisterRequest.cs
@@ -4,9 +4,11 @@ namespace VerifyEmailForgotPassword.Controllers.Models
 {
     public class UserRegisterRequest
     {
-        [Required]
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required, MinLength(8)]
         public string Password { get; set; } = string.Empty;
+        [Required,Compare("Password")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
